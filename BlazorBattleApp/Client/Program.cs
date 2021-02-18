14932544b1b527e0205e8cade84b6b5bc6d928a7
MunSeoJun <msj00130@gmail.com>
2021-02-18 17:35:11 +0900

    [Add] Unit Information Setup

--- BlazorBattleApp/Client/Program.cs
+++ BlazorBattleApp/Client/Program.cs
@@ -20,6 +20,7 @@ namespace BlazorBattleApp.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IBananaService, BananaService>();
+            builder.Services.AddScoped<IUnitService, IUnitService>();
 
             await builder.Build().RunAsync();
         }
